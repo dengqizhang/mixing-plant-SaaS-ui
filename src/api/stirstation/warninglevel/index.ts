a6471a7f2aad23068ccc6b9e4255e67be1d8f82e
dqzhang <2435563017@qq.com>
2024-06-13 11:10:02 +0800

    “预警人员等级管理模块完成”

--- src/api/stirstation/warninglevel/index.ts
+++ src/api/stirstation/warninglevel/index.ts
@@ -1,13 +1,39 @@
 import request from '@/utils/request';
 import { AxiosPromise } from 'axios';
-// import { MaterialsVO, MaterialsForm, MaterialsQuery } from '@/api/materials/materials/types';
+import { WarninglevelForm } from '@/api/materials/materials/types';
 /**
  * 获取对应预警级别的用户列表
  * @param id
  */
-export const selectwarninglevelById = () => {
+export const selectwarninglevelById = (id: string | number) => {
   return request({
-    url: '/warninglevel/warninglevel/list',
-    method: 'get'
+    url: '/warninglevel/warninglevel/' + id,
+    method: 'get',
   });
 };
+
+/**
+ * 修改对应预警级别的用户
+ * @param WarninglevelForm
+ */
+export const updateWarninglevel = (warninglevelList: string,warninglevelId: number) => {
+  return request({
+    url: '/warninglevel/warninglevel/edit',
+    method: 'post',
+    data: {
+      warninglevelList: warninglevelList,
+      warninglevelId: warninglevelId,
+    }
+  });
+};
+/**
+ * 获取用户列表
+ * @param id
+ */
+export const selectUserList = () => {
+  return request({
+    url: '/warninglevel/warninglevel/list',
+    method: 'get',
+  });
+};
+
--- src/views/stirstation/warninglevel/index.vue
+++ src/views/stirstation/warninglevel/index.vue
@@ -28,9 +28,9 @@
         <!-- 添加或修改材料管理对话框 -->
         <el-dialog :title="dialog.title" v-model="dialog.visible" width="500px" append-to-body>
             <template v-if="checkList.length > 0">
-                <el-checkbox-group v-model="warningLevelList">
-                    <el-checkbox v-for="item in checkList" :key="item.id" :label="item.userName"
-                        @change="checkboxChange(item.userId)">
+                <el-checkbox-group v-model="checkboxParams" @change="checkboxChange">
+                    <el-checkbox v-for="item in checkList" :key="item.index" :label="item.userId" v-model="item.userId">
+                        {{ item.userName }}
                     </el-checkbox>
                 </el-checkbox-group>
             </template>
@@ -48,29 +48,29 @@
 </template>
 
 <script setup name="Materials" lang="ts">
-import { selectwarninglevelById } from '@/api/stirstation/warninglevel';
-// const { proxy } = getCurrentInstance() as ComponentInternalInstance;
-let specificationTypeOptions = ref<any>([])
+import { selectwarninglevelById, updateWarninglevel, selectUserList } from '@/api/stirstation/warninglevel';
+const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 const materialsList = ref<any>([
     { id: 1, warninglevel: "初级" },
     { id: 2, warninglevel: "中级" },
     { id: 3, warninglevel: "高级" },
-]);
+]);  //表格数据
 const loading = ref(true);
-const checkList = ref([])
-const warningLevelList = ref([]);
+const checkList = ref([]) //多选框数据数组
 const total = ref(0);
+const warninglevelId = ref(); //预警等级id
 const buttonLoading = ref(false);
-const materialsFormRef = ref<ElFormInstance>();
+// const materialsFormRef = ref<ElFormInstance>();
+let checkboxParams = ref([]) //默认项数组
 const dialog = reactive<DialogOption>({
     visible: false,
     title: ''
 });
 
-const initFormData: MaterialsForm = {
+const initFormData: any = {
     id: undefined,
 }
-const data = reactive<PageData<MaterialsForm, MaterialsQuery>>({
+const data = reactive<any>({
     form: { ...initFormData },
     queryParams: {
         pageNum: 1,
@@ -79,63 +79,70 @@ const data = reactive<PageData<MaterialsForm, MaterialsQuery>>({
 });
 
 const { queryParams, form, rules } = toRefs(data);
+
+/**
+ * 多选框点击事件
+ */
 const checkboxChange = (value) => {
-    console.log(value);
+    checkboxParams.value = value;
+    console.log();
 
 }
-
 /** 查询材料管理列表 */
 const getList = () => {
     loading.value = true;
-    console.log(materialsList._rawValue);
-    total.value = materialsList._rawValue.length;
+    total.value = materialsList.value.length;
     loading.value = false;
 }
 
 /** 取消按钮 */
 const cancel = () => {
-    reset();
     dialog.visible = false;
+    // 清空默认项数组
+    reset();
+
 }
 /** 提交按钮 */
-const submitForm = () => {
-    console.log(warningLevelList.value);
-    //点击提交后将warningLevelList数组数据传入后端
+const submitForm = async () => {
+    // //点击提交后将checkboxParams数组转换数据传入后端
+    const res = await updateWarninglevel(JSON.stringify(checkboxParams.value), warninglevelId.value);
+    console.log(res);
+    if (res.code === 200) {
+        proxy?.$modal.msgSuccess("操作成功");
+        dialog.visible = false;
+        reset();
+    }
 }
 
 /** 表单重置 */
 const reset = () => {
-    form.value = { ...initFormData };
-    materialsFormRef.value?.resetFields();
+    checkboxParams.value = []
 }
 
-
-
-
-/** 修改按钮操作 */
-const handleUpdate = async (row?: MaterialsVO) => {
+/**
+ * 修改按钮操作
+ * @param row 表格当前行的id
+ */
+const handleUpdate = async (row?: any) => {
     console.log(row.id);
+    warninglevelId.value = row.id;
     //根据预警等级id查询对应的用户
-    const res = await selectwarninglevelById();
+    const res = await selectUserList();
     console.log(res);
-    //查询列表后，根据row.id和warninglevel做判断出回显数据，并创建新数组赋值给warningLevelList
-    let newWarningLevelList = res.rows.map(item => {
-        if (item.warninglevelId === row.id) {
-            return item.userName;
+    if (checkList.value != null) {
+        const warningLevelRes = await selectwarninglevelById(row.id);
+        if (warningLevelRes.data) {
+            let arr = JSON.parse(warningLevelRes.data)
+            console.log(arr);
+            checkboxParams.value = arr
+
         }
-    }).filter(item => item !== undefined);
-    console.log(newWarningLevelList);
-
-    warningLevelList.value = newWarningLevelList;
-    checkList.value = res.rows;
-    console.log(checkList.value.length);
-
-    reset();
+    }
+    // 编辑弹窗设置默认项数组
+    checkList.value = res.data;
     dialog.visible = true;
     dialog.title = "编辑";
 }
-
-
 
 onMounted(() => {
     getList();
